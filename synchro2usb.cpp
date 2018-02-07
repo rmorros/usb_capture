717f832efde4b1e039fa3b15264508607751102e
Ramon Morros <ramon.morros@upc.edu>
2018-02-07 23:26:24 +0100

    Some changes

--- synchro2usb.cpp
+++ synchro2usb.cpp
@@ -11,49 +11,11 @@
 
 #include <opencv2\opencv.hpp>
 #include <opencv\highgui.h>
+#include "synchro2usb.hpp"
 
 using namespace cv;
 using namespace std;
 
-#include <boost/cstdint.hpp> // see http://www.boost.org/doc/libs/1_50_0/libs/integer/doc/html/boost_integer/cstdint.html
-//#include <cstdint>         // should replace boost version with C++11?
-
-    // Define integer types (8, 16, 32, 64)
-
-    /*!
-     * \defgroup ImagePlusTypes Basic Data Types
-     */
-
-    //! Unsigned 8 bits integer  \ingroup ImagePlusTypes
-typedef boost::uint8_t        uint8;
-
-//! Signed 8 bits integer    \ingroup ImagePlusTypes
-typedef boost::int8_t         int8;
-    
-    //! Unsigned 16 bits integer \ingroup ImagePlusTypes
-    typedef boost::uint16_t       uint16;
-    
-    //! Signed 16 bits integer   \ingroup ImagePlusTypes
-    typedef boost::int16_t        int16;
-    
-    //! Unsigned 32 bits integer \ingroup ImagePlusTypes
-    typedef boost::uint32_t       uint32;
-    
-    //! Signed 32 bits integer   \ingroup ImagePlusTypes
-    typedef boost::int32_t        int32;
-
-//! Unsigned 64 bits integer \ingroup ImagePlusTypes
-typedef boost::uint64_t       uint64;
-
-//! Signed 64 bits integer   \ingroup ImagePlusTypes
-typedef boost::int64_t        int64;
-//#endif
-
-//! Signed 32 bits float     \ingroup ImagePlusTypes
-typedef float                 float32;
-
-//! Signed 64 bits float     \ingroup ImagePlusTypes
-typedef double                float64;
 
 
 
@@ -66,16 +28,17 @@ int main()
 
     // TODO: change the width, height, and capture FPS to your desired
     // settings.
-    cap.set(CAP_PROP_FRAME_WIDTH, 1920);
-    cap.set(CAP_PROP_FRAME_HEIGHT, 1080);
-    cap.set(CAP_PROP_FPS, 30);
+    cap.set(CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(CAP_PROP_FPS, 15);
 
     
-    Mat frameL, frameR;
+    std::vector<Mat> frameL, frameR;
 
     uint64 msecCounter = 0;
     uint64 frameNumber = 0;
     
+    uint64 counterL = 0, counterR = 0;
     
     while (1)
     {
@@ -99,25 +62,16 @@ int main()
             msecCounter = (uint64) captureL.get(CAP_PROP_POS_MSEC);
             frameNumber = (uint64) captureL.get(CAP_PROP_POS_FRAMES);
 
-            // VideoCapture::retrieve color converts the image and places
-            // it in the Mat that you provide.
-            if(captureL.retrieve(&frameL))
-            {
-                // Pass the frame and parameters to your processing
-                // method.
-                ProcessFrame(&frame, msecCounter, frameNumber);
-            }
+            // VideoCapture::retrieve color converts the image and places it in the Mat that you provide.
+            resL = captureL.retrieve(&frameL[counterL++]);
+            ...
         }
 
-        captureL >> imageL;
-        captureR >> imageR;
-        imshow("OriginalL", imageL);
-        imshow("OriginalR", imageR);
-        waitKey(1);
     }
-captureL.release();
-captureR.release();
-return 0;
+
+    captureL.release();
+    captureR.release();
+    return 0;
 }
 
 
