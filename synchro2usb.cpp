a87969360666e7cb2f912e1c305854f9e725282f
Josep Ramon Morros <ramon.morros@upc.edu>
2018-02-11 22:47:03 +0100

    1st working version

--- synchro2usb.cpp
+++ synchro2usb.cpp
@@ -9,12 +9,41 @@
 //!  Add brief description of the file here
 //!
 
-#include <opencv2\opencv.hpp>
-#include <opencv\highgui.h>
+//! https://stackoverflow.com/questions/44490743/how-to-extract-timestamps-from-each-frame-obtained-by-usb-camera
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <sys/time.h>
+
+#include <opencv2/opencv.hpp>
+//include <opencv/highgui.h>
 #include "synchro2usb.hpp"
 
 using namespace cv;
-using namespace std;
+
+
+// https://stackoverflow.com/questions/16357999/current-date-and-time-as-string
+std::string mytimestr(bool ms)
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+  char buffer[80];
+
+  time (&rawtime);
+  timeinfo = localtime(&rawtime);
+
+  strftime(buffer,sizeof(buffer),"%d-%m-%Y-%I-%M-%S",timeinfo);
+
+  if (ms)
+  {
+      struct timeval tv;
+      gettimeofday(&tv, NULL); 
+      sprintf(buffer, "%s-%d",buffer,tv.tv_usec/1000);
+  }
+
+  return std::string(buffer);
+}
 
 
 
@@ -22,31 +51,35 @@ using namespace std;
 int main()
 {
 
+    // TODO: change the width, height, and capture FPS to your desired
+    // settings.
     int width  = 640;
     int height = 480;
-    double fps =  25.0;
+    double fps =  25.0;  
 
     cv::VideoCapture captureL(0);
     if (!captureL.isOpened())
     {
-	std::cout << "L doesn't work" << std::endl;
-	return -1;
+        std::cout << "L doesn't work" << std::endl;
+        return -1;
     }
     VideoCapture captureR(1);
     if (!captureR.isOpened())
     {
-	std::cout << "R doesn't work" << std::endl;
-	return -1;
+        std::cout << "R doesn't work" << std::endl;
+        return -1;
     }
 
-    // TODO: change the width, height, and capture FPS to your desired
-    // settings.
-    cap.set(CAP_PROP_FRAME_WIDTH,  width);
-    cap.set(CAP_PROP_FRAME_HEIGHT, height);
-    cap.set(CAP_PROP_FPS, fps);
+    captureL.set(CAP_PROP_FRAME_WIDTH,  width);
+    captureL.set(CAP_PROP_FRAME_HEIGHT, height);
+    captureL.set(CAP_PROP_FPS, fps); // Does not work! Camera always captures at ~30fps
+
+    captureR.set(CAP_PROP_FRAME_WIDTH,  width);
+    captureR.set(CAP_PROP_FRAME_HEIGHT, height);
+    captureR.set(CAP_PROP_FPS, fps); // Does not work! Camera always captures at ~30fps
 
     
-    std::vector<cv::Mat> frameL, frameR;
+    cv::Mat frameL, frameR, frame;
 
     uint64 msecCounterL = 0;
     uint64 frameNumberL = 0;
@@ -55,7 +88,7 @@ int main()
     uint64 frameNumberR = 0;
     
     uint64 counterL = 0, counterR = 0;
-    std::string NAME = mytimestr();
+    std::string NAME = mytimestr(false);
 
     //int ex = -1; // Pop-up window asking for available codecs
     int codec = CV_FOURCC('M','J','P','G');
@@ -68,9 +101,14 @@ int main()
 
     long frame_num = 0;
 
-    while (1)
-    {
+    bool okL, okR, resL, resR;
 
+    int debug_count = 0;
+
+    std::cout << "TP" << debug_count++ << std::endl;
+    
+    for (;;)
+    {
         // Instead of cap >> frame; we'll do something different.
         //
         // VideoCapture::grab() tells OpenCV to grab a frame from
@@ -85,21 +123,73 @@ int main()
         // based on the capture FPS that is set, so it's okay to loop
         // continuously over it.
 
-        if(okL = captureL.grab())
+        okL = captureL.grab();
+        std::string stsL = mytimestr(true);
+        okR = captureR.grab();
+        std::string stsR = mytimestr(true);
+
+        std::cout << "TP" << debug_count++ << std::endl;
+        
+        if(okL && okR)
         {
- 	    std::string sts = mytimestr();
             msecCounterL = (uint64) captureL.get(CAP_PROP_POS_MSEC);
-            frameNumberL = (uint64) captureL.get(CAP_PROP_POS_FRAMES);
+            //frameNumberL = (uint64) captureL.get(CAP_PROP_POS_FRAMES);
+            msecCounterR = (uint64) captureR.get(CAP_PROP_POS_MSEC);
+            //frameNumberR = (uint64) captureR.get(CAP_PROP_POS_FRAMES);
 
-            // VideoCapture::retrieve color converts the image and places it in the Mat that you provide.
-            resL = captureL.retrieve(&frameL[counterL++]);
-            ...
+            std::cout << "TP" << debug_count++ << std::endl;
+
+            // VideoCapture::retrieve color converts the image and places
+            // it in the Mat that you provide.
+            resL = captureL.retrieve(frameL);
+            resR = captureR.retrieve(frameR);
+
+            std::cout << "TP" << debug_count++ << std::endl;
+
+            if (resL && resR)
+            {
+                cv::hconcat(frameL, frameR, frame);
+                outputVideo.write(frame);
+
+                // Save frame & timestamp to .ndx file
+                index_file << counterL++ << "\t" << counterR++ << "\t" << msecCounterL << "\t" << msecCounterR << "\t" << stsL << "\t" << stsR << std::endl;
+            }
+            else
+            {
+                if (resL)
+                {
+                    std::cerr << "R retrieval failed!" << std::endl;
+                }
+                else if (resR)
+                {
+                    std::cerr << "L retrieval failed!" << std::endl;
+                }
+                else
+                {                       
+                    std::cerr << "L and R retrieval failed!" << std::endl;
+                }
+            }
         }
-
+        else
+        {
+            if (okL)
+            {
+                std::cerr << "R capture failed!" << std::endl;
+            }
+            else if (okR)
+            {
+                std::cerr << "L capture failed!" << std::endl;
+            }
+            else
+            {                       
+                std::cerr << "L and R capture failed!" << std::endl;
+            }
+        }
     }
 
-    captureL.release();
-    captureR.release();
+    index_file.close();
+
+    // the cameras will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
 
