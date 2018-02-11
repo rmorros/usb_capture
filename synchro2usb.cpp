6d421009a97d859e5e961592e632b30cd44ae573
Josep Ramon Morros <ramon.morros@upc.edu>
2018-02-11 21:05:43 +0100

    more modifications

--- synchro2usb.cpp
+++ synchro2usb.cpp
@@ -21,25 +21,53 @@ using namespace std;
 
 int main()
 {
-    VideoCapture captureL(0);
-    if (!captureL.isOpened()) cout << "L doesn't work" << endl;
+
+    int width  = 640;
+    int height = 480;
+    double fps =  25.0;
+
+    cv::VideoCapture captureL(0);
+    if (!captureL.isOpened())
+    {
+	std::cout << "L doesn't work" << std::endl;
+	return -1;
+    }
     VideoCapture captureR(1);
-    if (!captureR.isOpened()) cout << "R doesn't work" << endl;
+    if (!captureR.isOpened())
+    {
+	std::cout << "R doesn't work" << std::endl;
+	return -1;
+    }
 
     // TODO: change the width, height, and capture FPS to your desired
     // settings.
-    cap.set(CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
-    cap.set(CAP_PROP_FPS, 15);
+    cap.set(CAP_PROP_FRAME_WIDTH,  width);
+    cap.set(CAP_PROP_FRAME_HEIGHT, height);
+    cap.set(CAP_PROP_FPS, fps);
 
     
-    std::vector<Mat> frameL, frameR;
+    std::vector<cv::Mat> frameL, frameR;
 
-    uint64 msecCounter = 0;
-    uint64 frameNumber = 0;
+    uint64 msecCounterL = 0;
+    uint64 frameNumberL = 0;
+
+    uint64 msecCounterR = 0;
+    uint64 frameNumberR = 0;
     
     uint64 counterL = 0, counterR = 0;
-    
+    std::string NAME = mytimestr();
+
+    //int ex = -1; // Pop-up window asking for available codecs
+    int codec = CV_FOURCC('M','J','P','G');
+
+    VideoWriter outputVideo;
+    Size S = Size(width, height);
+    outputVideo.open(NAME+".avi" , codec, fps, S, true);
+
+    std::ofstream index_file(NAME.c_str(), std::ofstream::out);
+
+    long frame_num = 0;
+
     while (1)
     {
 
@@ -57,10 +85,11 @@ int main()
         // based on the capture FPS that is set, so it's okay to loop
         // continuously over it.
 
-        if(captureL.grab())
+        if(okL = captureL.grab())
         {
-            msecCounter = (uint64) captureL.get(CAP_PROP_POS_MSEC);
-            frameNumber = (uint64) captureL.get(CAP_PROP_POS_FRAMES);
+ 	    std::string sts = mytimestr();
+            msecCounterL = (uint64) captureL.get(CAP_PROP_POS_MSEC);
+            frameNumberL = (uint64) captureL.get(CAP_PROP_POS_FRAMES);
 
             // VideoCapture::retrieve color converts the image and places it in the Mat that you provide.
             resL = captureL.retrieve(&frameL[counterL++]);
